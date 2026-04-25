16b9b3d517edeb2452f7a42287fdbba4cfc7efd1
Brendan Goubin <brendan.goubin@free.fr>
2026-04-25 15:54:14 +0200

    fix: hangle error

--- src/controllers/wud.ts
+++ src/controllers/wud.ts
@@ -5,24 +5,29 @@ import { getContainers } from "../sdk/wud/client"
 import { FormattedEvent } from "../types/gotify"
 
 export async function handleWudRequest(_req: Request<{}, {}, {}>, res: Response<{}, { data: FormattedEvent }>) {
-  const appMessages = await getApplicationMessages()
-  const containers = await getContainers()
-  const upToDateContainers = containers.filter((c) => !c.updateAvailable)
+  try {
+    const appMessages = await getApplicationMessages()
+    const containers = await getContainers()
+    const upToDateContainers = containers.filter((c) => !c.updateAvailable)
 
-  for (const container of upToDateContainers) {
-    const messages = appMessages.messages.filter((m) => m.message.includes(container.name))
-    if (messages.length === 0) {
-      continue
-    }
-    console.log(`Found ${messages.length} notifications for container ${container.name}`)
-    for (const message of messages) {
-      try {
-        await deleteMessage(message.id)
-      } catch (error) {
-        console.error(`Failed to delete message ${message.id} for container ${container.name}:`, error)
+    for (const container of upToDateContainers) {
+      const messages = appMessages.messages.filter((m) => m.message.includes(container.name))
+      if (messages.length === 0) {
+        continue
+      }
+      console.log(`Found ${messages.length} notifications for container ${container.name}`)
+      for (const message of messages) {
+        try {
+          await deleteMessage(message.id)
+        } catch (error) {
+          console.error(`Failed to delete message ${message.id} for container ${container.name}:`, error)
+        }
       }
     }
-  }
 
-  return res.status(200).send("OK")
+    return res.status(200).send("OK")
+  } catch (error) {
+    console.error("Failed to handle WUD request:", error)
+    return res.status(500).send("Internal Server Error")
+  }
 }
